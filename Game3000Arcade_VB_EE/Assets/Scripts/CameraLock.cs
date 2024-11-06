8681fc0636b36f84f156c41f901b1df5b449d4c2
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2024-11-06 00:35:47 -0500

    why unity why
    i am loosing my marbles

--- Game3000Arcade_VB_EE/Assets/Scripts/CameraLock.cs
+++ Game3000Arcade_VB_EE/Assets/Scripts/CameraLock.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraLock : MonoBehaviour
 {
+    private GameObject p1Cam;
+    private GameObject p2Cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1 = GameObject.Find("Player1Cam");
+        cam2 = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void
 }
